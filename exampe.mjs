98f3b83ccae41ff4dcc5f1e1ed688c933372f266
Adrian Rudnik <adrian@klonmaschine.de>
2021-06-20 16:22:30 +0200

    Cleaned example code

--- exampe.mjs
+++ exampe.mjs
@@ -1,11 +1,8 @@
 import { readCertificate, readQrCode } from './lib.mjs'
 
-let qrCode = 'HC1:...'
+const qrCode = 'HC1:...'
 
 const decoded = await readQrCode(qrCode)
 
 console.log('Signed by ' + decoded.issuer + ' @ ' + decoded.country)
-
-const humanReadable = readCertificate(decoded.content)
-
-console.log('HUMAN', humanReadable)
+console.log(readCertificate(decoded.content))
